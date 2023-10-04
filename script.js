194aab60241cb41e1e2003a303a7334c7cdb0e01
Bavy89 <85221462+Bavy89@users.noreply.github.com>
2023-10-04 19:24:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
